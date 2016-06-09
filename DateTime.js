15da03ed0d04f085edc8672c1eb06a68d0843ef7
Jon Araquistain <jon@eero.com>
2016-06-08 22:30:55 -0700

    Fetch new props if open changes

--- DateTime.js
+++ DateTime.js
@@ -142,7 +142,7 @@ var Datetime = React.createClass({
 			update = {}
 		;
 
-		if( nextProps.value != this.props.value ){
+		if( nextProps.value != this.props.value || nextProps.open != this.props.open ){
 			update = this.getStateFromProps( nextProps );
 		}
 		if ( formats.datetime !== this.getFormats( this.props ).datetime ) {
